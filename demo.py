4e1cf675980e500b539a9eed9678cbd2cc52b1dd
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2024-01-11 15:14:35 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,11 +9,12 @@ if __name__ == '__main__':
     st.title("STOCK PREDICTION")
     st.markdown('<h3 style="color: red"> USING LSTM </h3', unsafe_allow_html=True)
     st.sidebar.text("Input your information")
-    st.sidebar.markdown("---")
     stock_symbol = st.sidebar.text_input("Enter Stock Symbol")
+    st.sidebar.markdown("---")
     open_price = st.sidebar.number_input("Enter the price at the time of market opening")
     high_price = st.sidebar.number_input("Enter the high price of stock")
     low_price = st.sidebar.number_input("Enter the low price")
+    st.sidebar.markdown("---")
     present_price = st.sidebar.number_input("Enter the present price")
     volume = st.sidebar.number_input("Enter the volume at present")
 
