282227571d20f5560f3d1bcd64b532f563e099dc
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2024-01-11 14:56:07 +0700

    change app.py to demo.py

---
+++ demo.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow
+import streamlit as st
+
+# Load pre-trained model
+model = tensorflow.keras.models.load_model("model_predict_stock.h5", compile=False)
+st.title("STOCK PREDICTION USING LSTM")
