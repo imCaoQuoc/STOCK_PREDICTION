7d68d5b0c62d24b0c0146ab262b642ddac11fa50
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2024-01-11 15:06:11 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,8 +4,11 @@ import streamlit as st
 
 # Load pre-trained model
 model = tensorflow.keras.models.load_model("model_predict_stock.h5", compile=False)
-st.title("STOCK PREDICTION")
-st.markdown('<h3 style="color: red"> USING LSTM </h3', unsafe_allow_html=True)
 
+if __name__ == '__main__':
+    st.title("STOCK PREDICTION")
+    st.markdown('<h3 style="color: red"> USING LSTM </h3', unsafe_allow_html=True)
+    st.sidebar.text("Input your information")
+    st.sidebar.markdown("---")
+    st.sidebar.text_input("Input Stock Sympol")
 
-st.sidebar.text('Input data')
