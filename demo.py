bd7ccb643c64e5c031a6525edbe3cb8f77d124d4
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2024-01-11 15:14:58 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ model = tensorflow.keras.models.load_model("model_predict_stock.h5", compile=Fal
 if __name__ == '__main__':
     st.title("STOCK PREDICTION")
     st.markdown('<h3 style="color: red"> USING LSTM </h3', unsafe_allow_html=True)
-    st.sidebar.text("Input your information")
+    st.sidebar.text("Your Stock Information")
     stock_symbol = st.sidebar.text_input("Enter Stock Symbol")
     st.sidebar.markdown("---")
     open_price = st.sidebar.number_input("Enter the price at the time of market opening")
